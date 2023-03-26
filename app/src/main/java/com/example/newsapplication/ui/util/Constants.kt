aebb2ed5e1dd689f96b756ea7e1d46297f709430
BIKASH <samantaraybikash2015@gmail.com>
2023-03-27 01:17:44 +0530

    do it or die

---
+++ app/src/main/java/com/example/newsapplication/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapplication.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "9c2c347980644a37aba9d3018badf4e9"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
